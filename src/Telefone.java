f6c9a34ace0b0f619ce615024996d70f6fa76d45
Yuri Souza <101481299+Souza1999@users.noreply.github.com>
2023-08-03 11:20:26 -0300

    Apenas criando o arquivo java

---
+++ src/Telefone.java
@@ -0,0 +1,10 @@
+public class Telefone {
+	private String numero;
+	private Pessoa pessoa;
+	
+	public Telefone(String numero, Pessoa pessoa) {
+		this.numero = numero;
+		this.pessoa = pessoa;
+	}
+	
+}
